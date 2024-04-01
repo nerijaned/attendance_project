de293eecc66ba682e6621bbc17eee45259550a07
nerijaned <nerizzajane.dejesus@gmail.com>
2024-04-01 11:37:59 -0600

    1st commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+require('dotenv').config;
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=>{
+    console.log(`Successfully connected to ${PORT}`);
+});
