7bb8d0ed8f2fee521b6e514fb0e11d17a4a2b5db
nerijaned <nerizzajane.dejesus@gmail.com>
2024-04-02 11:50:20 -0600

    Second commit

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const express = require('express');
 require('dotenv').config;
+const mongoose = require('mongoose');
 const app = express();
+
+app.set('view engine','ejs');
+app.set('views','./views');
+
+app.use(express.static('public'));
+
+const url = `mongodb+srv://testUser:Password@cluster0.q5sujkp.mongodb.net/`;
+
+
+mongoose.connect(url)
+.then(() =>{
+    console.log('Connected to MongoDB Database');
+})
+.catch((err)=>{
+    console.log(`Error connecting to the database: ${err}`)
+});
 
 const PORT = process.env.PORT || 3000;
 
