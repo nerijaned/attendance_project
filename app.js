a429a76a8e0ad6546c353b37aae4be8bf7c5af99
nerijaned <nerizzajane.dejesus@gmail.com>
2024-04-03 11:45:50 -0600

    Commit 5

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 require('dotenv').config;
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const cookieParser = require('cookie-parser');
+
 const app = express();
 
 app.set('view engine','ejs');
@@ -10,6 +14,9 @@ app.use(express.static('public'));
 
 const url = `mongodb+srv://testUser:Password@cluster0.q5sujkp.mongodb.net/`;
 
+app.get('/',(req,res) => {
+    res.render('login');
+});
 
 mongoose.connect(url)
 .then(() =>{
